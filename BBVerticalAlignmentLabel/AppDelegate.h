5fd56ee5d22ac1eebed01be4cc6218e78ebdd3f4
liukun <765409243@qq.com>
2014-02-14 22:53:53 +0800

    Initial Commit

---
+++ BBVerticalAlignmentLabel/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BBVerticalAlignmentLabel
+//
+//  Created by liukun on 14-2-14.
+//  Copyright (c) 2014年 liukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
